7a3e91a83d748f2da4dc2eb50b55b9255c796620
Your Name <your-email@example.com>
2024-12-31 12:40:23 +0530

    Updated settings for production

--- my_project/wsgi.py
+++ my_project/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for my_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the settings module for the Django project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 
+# Get the WSGI application
 application = get_wsgi_application()
